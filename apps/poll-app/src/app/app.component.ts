4a3b57dd0a180c79e4bcb841b5ec8f24317103f4
Bartek Wasilew <bartek@angulion.dev>
2024-02-15 20:55:05 +0100

    feat(landing): include non-commercial disclaimer & final enhancements

--- apps/poll-app/src/app/app.component.ts
+++ apps/poll-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '@header';
@@ -7,13 +7,30 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MailingComponent } from '@shared/mailing';
 
+import aos from 'aos';
+
 @Component({
   selector: 'tr-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, NgOptimizedImage, MatButtonModule, FormsModule, MatInputModule, RouterLink, ReactiveFormsModule, MailingComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    NgOptimizedImage,
+    MatButtonModule,
+    FormsModule,
+    MatInputModule,
+    RouterLink,
+    ReactiveFormsModule,
+    MailingComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ng-tech-radar-poll';
+export class AppComponent implements OnInit {
+  title = 'Angular Tech Radar';
+
+  ngOnInit() {
+    aos.init();
+  }
 }
--- libs/landing/src/lib/landing/landing.component.ts
+++ libs/landing/src/lib/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { MailingComponent } from '@shared/mailing';
 import { MatButtonModule } from '@angular/material/button';
@@ -8,12 +8,19 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'lib-landing',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, NgOptimizedImage, RouterLink, MailingComponent, MatIconModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    NgOptimizedImage,
+    RouterLink,
+    MailingComponent,
+    MatIconModule,
+  ],
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss', './landing.component.mobile.scss'],
 })
 export class LandingComponent {
   protected scrollTo(el: HTMLElement): void {
-    el.scrollIntoView({behavior: 'smooth', block: 'center'});
+    el.scrollIntoView({ behavior: 'smooth', block: 'center' });
   }
 }
